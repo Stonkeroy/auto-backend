e2c4f656f6167254409353b74a3cad410cb14811
Stonkeroy <fraimer.02.ua@gmail.com>
2026-06-16 11:27:15 +0300

    fix import case sensitivity

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,3 @@
 export * as UserController from './UserController.js'
 export * as CarController from './CarController.js'
-export * as AiController from './AiController.js'
+export * as AiController from './AIController.js'
